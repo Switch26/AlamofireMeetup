f7da945a9dc1ae47f7bc3b56a55b3ac346868d86
Serguei Vinnitskii <serguei.vinnitskii@gmail.com>
2017-02-15 16:33:12 -0500

    deleted extensions

--- MeetupAlamofire/Extensions.swift
+++ MeetupAlamofire/Extensions.swift
@@ -9,34 +9,4 @@
 import Foundation
 import UIKit
 
-extension UIAlertController {
-    
-    class func simpleAlert(withTitle title: String, message: String) -> UIAlertController {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
-        }
-        alertController.addAction(OKAction)
-        return alertController
-    }
-}
 
-
-extension UIViewController {
-    func handleNetworkManagerErrors(networkManagerError: APICallError) {
-        
-        switch networkManagerError {
-        case .networkError(let networkErrorDescription):
-            let networkErrorAlert = UIAlertController.simpleAlert(withTitle: "Error", message: networkErrorDescription)
-            self.present(networkErrorAlert, animated: true, completion: nil)
-        case .serverError:
-            let serverErrorAlert = UIAlertController.simpleAlert(withTitle: "Error", message: "Server didn't respond")
-            self.present(serverErrorAlert, animated: true, completion: nil)
-        case .parsingJSONError:
-            break
-        }
-    }
-}
-
-
-// Usage:
-// self.handleNetworkManagerErrors(ApiError: APICallError)
