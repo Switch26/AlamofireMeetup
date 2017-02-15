da56094546b61d85d71e3defc7dd3f076592c377
Serguei Vinnitskii <serguei.vinnitskii@gmail.com>
2017-02-15 18:40:35 -0500

    initial commit

--- MeetupAlamofire/AlamofireManager.swift
+++ MeetupAlamofire/AlamofireManager.swift
@@ -14,13 +14,23 @@ struct AlamoFireManager {
     static func searchMovies(withKeyword keyword: String, completionHandler: @escaping (Any)-> Void) {
         
         let url = APIGlobalUrl + "?s=\(keyword)&y=&plot=&r=json"
-        
-        Alamofire.request(url).responseJSON { response in
+
+        Alamofire.request(url).validate().responseJSON { response in
             
-            if let JSON = response.result.value {
-                completionHandler(JSON)
+            let responseCode = response.response?.statusCode
+            print("response code: \(responseCode)")
+            
+            switch response.result {
+            case .success:
+                print("Validation successful")
+                if let JSON = response.result.value {
+                    completionHandler(JSON)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
             }
         }
+        
     }
 
 }
