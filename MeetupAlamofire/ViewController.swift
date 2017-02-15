43db76bfc0d1a3264d05dc6c73544e32a9afa87b
Serguei Vinnitskii <serguei.vinnitskii@gmail.com>
2017-02-15 17:33:26 -0500

    initial commit

--- MeetupAlamofire/ViewController.swift
+++ MeetupAlamofire/ViewController.swift
@@ -21,6 +21,14 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        URLSessionManager.searchMovies(forKeyword: "Matrix") { (jsonObject: [String : Any]) in
+            
+            DispatchQueue.main.async {
+                self.textView.text = jsonObject.description
+
+            }
+        }
+        
     }
 
 }
